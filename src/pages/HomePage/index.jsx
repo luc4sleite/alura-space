59c2c5e356d2b313d05b4e49155ecb3f331b5970
luc4sleite <lucahleite@gmail.com>
2023-04-19 15:07:52 -0300

    feat: banner

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,11 +1,19 @@
+import Banner from "../../components/Banner";
 import Header from "../../components/Header";
 import Menu from "../../components/Menu";
+import styles from './HomePage.module.scss'
 
 export default function HomePage() {
     return (
         <>
             <Header/>
-            <Menu />
+            <main>
+                <section className={styles.principal}>
+                    <Menu />
+                    <Banner />
+                </section>
+            </main>
+            
         </>
     )
 }
